4e9dc5045c2bd35eeb47c1f8fb83878c7a0e48c6
jona62 <jonathanjamesm66@gmail.com>
2018-04-20 23:10:59 -0400

    Assignment4

--- Assignment4/Assignment4.cpp
+++ Assignment4/Assignment4.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 int Phi(int);
+int GCD(int, int);
 
 int main(){
   fstream out;
@@ -20,17 +21,14 @@ int main(){
 
 int Phi(int n){
   int count=0;
-  for(int i=1; i<n; i++){
-       bool isPrime=true;
-       for (int j=2; j*j<=i; j++){
-           if (i % j == 0){
-               isPrime=false;
-               break;
-           }
-       }
-       if(isPrime){
-         count++;
-       }
-   }
+  for(int i=1; i<=n; i++){
+    if(GCD(n,i) == 1){
+      count++;
+    }
+  }
   return count;
+}
+
+int GCD(int num1, int num2){
+  return (num2 != 0)? GCD(num2, num1 % num2): num1;
 }
